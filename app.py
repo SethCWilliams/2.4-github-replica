9fdd5d3ddebf9747c351f08ee2249e47b2872155
Seth Williams <swilliams9334@gmail.com>
2019-01-18 10:22:47 -0500

    Bio is moving to page, but can't figure out image. Want to get that first. Everything else will fall into place after. At least on the user info request.

--- app.py
+++ app.py
@@ -10,7 +10,32 @@ from flask import render_template
 app = Flask(__name__)
 
 
+response = requests.get('https://avatars2.githubusercontent.com/u/43652084?v=4')
+
+
+
+user_data = requests.get("https://api.github.com/users/SethCWilliams")
+decoded = user_data.json()
+put_together = decoded['bio']
+avatar = decoded ['avatar']
+
+# def base():
+#     return render_template('base.html', bio=put_together)
+#ask why this isn't working outside of the app route, but it worked while in it
+repos = requests.get("https://api.github.com/users/SethCWilliams/repos")
+repo_info = repos.json()
+
+@app.route("")
+def base():
+    return render_template('base.html', bio=put_together, avatar=avatar)
+
 @app.route("/")
+# def base():
+#     return render_template('base.html', bio=put_together, avatar=response)
 def index():
 
+
     return render_template('index.html')
+
+
+# avatar=response
