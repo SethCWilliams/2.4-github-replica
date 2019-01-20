20d018af0f20b2923c10381a851c6a7e5b29a62a
Seth Williams <swilliams9334@gmail.com>
2019-01-19 22:14:42 -0500

    Started messing around with css. Header is pretty close. There has to be a better way than trial and error. The pixel measuring isn't working... at least with how I'm doing it.

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import csv
+github_api_token = '8d23ff8b9838003b71cade8d2acdb857ebb86ff8'
+headers = {
+    'Authorization': 'token {}'.format(github_api_token)
+}
+
+
 
 from flask import Flask
 from flask import request
@@ -10,10 +16,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    user_data = requests.get("https://api.github.com/users/SethCWilliams")
+    user_data = requests.get("https://api.github.com/users/SethCWilliams", headers=headers)
     decoded = user_data.json()
 
-    repos = requests.get("https://api.github.com/users/SethCWilliams/repos")
+    repos = requests.get("https://api.github.com/users/SethCWilliams/repos", headers=headers)
     repo_info = repos.json()
 
     context = {
@@ -33,19 +39,42 @@ def index():
 
 @app.route("/followers.html")
 def followers():
-    user_data = requests.get("https://api.github.com/users/SethCWilliams")
+    user_data = requests.get("https://api.github.com/users/SethCWilliams", headers=headers)
     decoded = user_data.json()
 
-    folower_data = requests.get("https://api.github.com/users/SethCWilliams/followers")
-    decode = folower_data.json()
+    follower_data = requests.get("https://api.github.com/users/SethCWilliams/followers", headers=headers)
+    decode = follower_data.json()
+    #new test is equal to all of the urls that I need to get api info on.
+    #I need to ask why this works. I get the idea of it, but I took it from someone online.
+    new_test = [user_info['login'] for user_info in decode]
 
-    context = {
+    user_info_list = []
+
+    for item in new_test:
+        item2 = requests.get("https://api.github.com/users/{}".format(item), headers=headers)
+        decoderama = item2.json()
+        #I had to append this to a list because decoderama only lives in this function
+        user_info_list.append(decoderama)
+
+
+    ctxt = {
         'bio': decoded['bio'],
         'avatar': decoded['avatar_url'],
         'name': decoded['name'],
         'location': decoded['location'],
         'email': decoded['email'],
-        'followers': decode
+        'user_filler': user_info_list
     }
-    return render_template('followers.html', **context)
+
+
+    print(user_info_list)
+
+
+    return render_template('followers.html', **ctxt)
 # avatar=response
+
+
+
+
+if __name__ == '__main__':
+    app.run()
